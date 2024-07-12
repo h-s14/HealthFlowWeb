4c1fe85777e096abae59fff44d052bff7059d6b5
Harry <14singh.harkirat@gmail.com>
2024-07-13 01:15:28 +0530

    Initial commit

---
+++ BackEnd/models/userSchema.js
@@ -0,0 +1,79 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: [3, "First Name must contain at least 3 characters"],
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: [3, "Last Name must contain at least 3 characters"],
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Please provide a vaild e-mail"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    minLength: [11, "Phone number must be 11-Digits"],
+    maxLength: [11, "Phone number must be 11-Digits"],
+  },
+  nic: {
+    type: String,
+    required: true,
+    minLength: [5, "NIC must be 5-Digits"],
+    maxLength: [5, "NIC must be 5-Digits"],
+  },
+  dob: {
+    type: Date,
+    required: [true, "DOB is required"],
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female", "Prefer Not Say"],
+  },
+  password: {
+    type: String,
+    minLength: [8, "Password must contain at least 8 characters"],
+    required: true,
+    select: false,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["Admin", "Patient", "Doctor"],
+  },
+  doctorDepartment: {
+    type: String,
+  },
+  docAvatar: {
+    public_id: String,
+    url: String,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.passwordChecker = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.generateJsonWebToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: JWT_EXPIRES,
+  });
+};
+export const User = mongoose.model("User", userSchema);
